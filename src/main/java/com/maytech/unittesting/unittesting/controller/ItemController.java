48c28c2ac377de0e772f4dc584c9c00b353d7c26
maycem <maycemamor@gmail.com>
2024-05-02 17:11:04 +0200

    - Creating a Basic REST Service in Item Controller.

---
+++ src/main/java/com/maytech/unittesting/unittesting/controller/ItemController.java
@@ -0,0 +1,13 @@
+package com.maytech.unittesting.unittesting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/maycem-item")
+    Item amorItem() {
+        return new Item(1, "Ball", 10, 100);
+    }
+}
