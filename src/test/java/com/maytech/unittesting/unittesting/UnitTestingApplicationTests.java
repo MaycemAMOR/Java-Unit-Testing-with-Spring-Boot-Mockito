5e1198c99affa986b25f7f7674119fdfda05f238
maycem <maycemamor@gmail.com>
2024-05-03 12:02:01 +0200

    - Tip - Creating Different Test Configuration.

--- src/test/java/com/maytech/unittesting/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/com/maytech/unittesting/unittesting/UnitTestingApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//@TestPropertySource(locations = {"classpath:test-configuration.properties"})
+// on peut spécifier une configuration lié au test en créant le fichier test-configuration.properties dans les ressources des tests
 class UnitTestingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
