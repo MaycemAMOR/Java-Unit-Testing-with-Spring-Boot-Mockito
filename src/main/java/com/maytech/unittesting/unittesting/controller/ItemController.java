b2cccb629d079a1531351e2eda42c8fa0388c7d1
maycem <maycemamor@gmail.com>
2024-05-03 15:24:35 +0200

    - add :  - createItem (post) URI : /items,  - retrieveItemById (get) URI: /items/{id},  - retrieveAllItems(get) URI: /items.

--- src/main/java/com/maytech/unittesting/unittesting/controller/ItemController.java
+++ src/main/java/com/maytech/unittesting/unittesting/controller/ItemController.java
@@ -2,10 +2,13 @@ package com.maytech.unittesting.unittesting.controller;
 
 import com.maytech.unittesting.unittesting.business.ItemBusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ItemController {
@@ -29,4 +32,33 @@ public class ItemController {
         items.forEach(item -> item.setValue(item.getPrice() * item.getQuantity()));
         return items;
     }
+
+
+    @PostMapping("/items")
+    public ResponseEntity<Item> createItem(@RequestBody Item item) {
+        Item savedItem = businessService.createItem(item);
+        // envoyer location aprés la création de chaque item
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedItem.getId())
+                .toUri();
+        return ResponseEntity.created(location).build(); /* amiliorer le code status de retour sucess response => 200
+                                                            created status => 201
+                                                            no content => 204
+                                                            unauthorized (when authorization fails) => 401
+                                                            Bad Request (validation error) => 400
+                                                            resourcer is not found => 404
+                                                            server exception => 500*/
+    }
+
+    @GetMapping("/items")
+    public List<Item> retrieveAllItems() {
+        return businessService.retrieveAllItem();
+    }
+
+    @GetMapping("/items/{id}")
+    public Optional<Item> retrieveItemById(@PathVariable Integer id) {
+        return businessService.retrieveItemById(id);
+    }
+
 }
