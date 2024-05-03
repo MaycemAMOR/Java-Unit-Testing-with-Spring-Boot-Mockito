5516506c4b05ee37b9cf60d09dd4979d041c5ca7
maycem <maycemamor@gmail.com>
2024-05-03 15:20:33 +0200

    - add createItem and retrieveItemById in the ItemBusinessService.java.

--- src/main/java/com/maytech/unittesting/unittesting/business/ItemBusinessService.java
+++ src/main/java/com/maytech/unittesting/unittesting/business/ItemBusinessService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ItemBusinessService {
@@ -19,6 +20,14 @@ public class ItemBusinessService {
 
     public Item retrieveHardCodedItem() {
         return new Item(1, "Ball", 10, 100);
+    }
+
+    public Item createItem(Item item) {
+        return repository.save(item);
+    }
+
+    public Optional<Item> retrieveItemById(Integer id) {
+        return repository.findById(id);
     }
 
 }
